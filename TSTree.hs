e5d743d65eb0d056ab11ff866cb3c612ceaac945
ahihi <violetmaze@gmail.com>
2012-11-26 22:05:08 +0200

    refactor insert

--- TSTree.hs
+++ TSTree.hs
@@ -9,17 +9,15 @@ empty :: TSTree k v
 empty = Empty
 
 insert :: (Ord k) => [k] -> v -> TSTree k v -> TSTree k v
-insert []     _  t                    = t
-insert [x]    v' Empty                = Node x (Just v') Empty Empty Empty
-insert [x]    v' (Node k mv lo eq hi)
-    | x < k                           = Node k mv (insert [x] v' lo) eq hi
-    | x > k                           = Node k mv lo eq (insert [x] v' hi)
-    | otherwise                       = Node k (Just v') lo eq hi
-insert (x:xs) v' Empty                = Node x Nothing Empty (insert xs v' Empty) Empty
-insert (x:xs) v' (Node k mv lo eq hi)
-    | x < k                           = Node k mv (insert (x:xs) v' lo) eq hi
-    | x > k                           = Node k mv lo eq (insert (x:xs) v' hi)
-    | otherwise                       = Node k mv lo (insert xs v' eq) hi
+insert []        _  t                    = t
+insert [x]       v' Empty                = Node x (Just v') Empty Empty Empty
+insert (x:x1:xs) v' Empty                = Node x Nothing Empty (insert (x1:xs) v' Empty) Empty
+insert (x:xs)    v' (Node k mv lo eq hi)
+    | x < k                              = Node k mv (insert (x:xs) v' lo) eq hi
+    | x > k                              = Node k mv lo eq (insert (x:xs) v' hi)
+    | otherwise = case xs of
+        []                              -> Node k (Just v') lo eq hi
+        _:_                             -> Node k mv lo (insert xs v' eq) hi
 
 find :: (Ord k) => [k] -> TSTree k v -> Maybe v
 find _      Empty                = Nothing
